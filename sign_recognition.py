48f999745bf8665835d15b0ca227598784954227
Hamid Reza <hr.roodabeh@gmail.com>
2018-08-31 22:36:14 +0430

    a 3-layer ff network with tf

--- sign_recognition.py
+++ sign_recognition.py
@@ -25,8 +25,8 @@ def convert_to_one_hot(Y, C):
     return Y
 
 def create_placeholders(n_x, n_y):
-    X = tf.placeholder(tf.float32, [n_x])
-    Y = tf.placeholder(tf.float32, [n_y])    
+    X = tf.placeholder(tf.float32, [n_x, None], name= 'X')
+    Y = tf.placeholder(tf.float32, [n_y, None], name= 'Y')    
     return X, Y
 
 def initialize_parameters():
@@ -49,9 +49,95 @@ def initialize_parameters():
     return parameters
 
 
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+    
+    Z1 = tf.add(tf.matmul(W1, X), b1)
+    A1 = tf.nn.relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)
+    A2 = tf.nn.relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)
+    
+    return Z3
 
 
+def compute_cost(Z3, Y):
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+    
+    return cost
 
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
+          num_epochs = 1500, minibatch_size = 32, print_cost = True):
+    
+    tf.set_random_seed(1)                             
+    seed = 3                                         
+    (n_x, m) = X_train.shape                          
+    n_y = Y_train.shape[0]                            
+    costs = []                                        
+    
+    X, Y = create_placeholders(n_x, n_y)
+
+    parameters = initialize_parameters()
+    
+    Z3 = forward_propagation(X, parameters)
+    
+    cost = compute_cost(Z3, Y)
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+    
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        
+        sess.run(init)
+        
+        for epoch in range(num_epochs):
+
+            epoch_cost = 0.
+            num_minibatches = int(m / minibatch_size)
+            seed = seed + 1
+            minibatches = generate_random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+
+                (minibatch_X, minibatch_Y) = minibatch
+                
+                _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={
+                    X : minibatch_X, 
+                    Y : minibatch_Y
+                })
+                
+                epoch_cost += minibatch_cost / num_minibatches
+
+            if print_cost == True and epoch % 100 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, epoch_cost))
+            if print_cost == True and epoch % 5 == 0:
+                costs.append(epoch_cost)
+                
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        parameters = sess.run(parameters)
+        print ("Parameters have been trained!")
+
+        correct_prediction = tf.equal(tf.argmax(Z3), tf.argmax(Y))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print ("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
+        print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+        
+        return parameters
 
 
 
@@ -73,4 +159,4 @@ if __name__ == '__main__':
     Y_train = convert_to_one_hot(Y_train_orig, 6)
     Y_test = convert_to_one_hot(Y_test_orig, 6)
 
-    print(test_pic[0][0])
+    parameters = model(X_train, Y_train, X_test, Y_test)
