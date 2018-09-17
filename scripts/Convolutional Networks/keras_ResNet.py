8b4e99056fee92ff54bf94e81b1f8d8378f9fc00
Hamid Reza <hr.roodabeh@gmail.com>
2018-09-17 22:40:37 +0430

    ResNet50 implementation

--- scripts/Convolutional Networks/keras_ResNet.py
+++ scripts/Convolutional Networks/keras_ResNet.py
@@ -20,9 +20,39 @@ K.set_image_data_format('channels_last')
 K.set_learning_phase(1)
 
 from keras_initiative import load_dataset
-from deep_utils import generate_random_mini_batches
 from conv_net_tensorflow import one_hot as convert_to_one_hot
+import math
 # ------------------------------------------------------------------------------------------------------ #
+
+
+def random_mini_batches(X, Y, minibatch_size, seed):
+
+    np.random.seed(seed)
+    m = X.shape[1]
+    indices = list(np.random.permutation(m))
+
+    full_minibatch_count = math.floor(m/minibatch_size)
+
+    X_shuffled = X[:, indices]
+    Y_shuffled = Y[:, indices]
+
+    mini_batches = []
+
+    for i in range(full_minibatch_count):
+        X_minibatch = X_shuffled[:, i*minibatch_size:(i+1)*minibatch_size]
+        Y_minibatch = Y_shuffled[:, i*minibatch_size:(i+1)*minibatch_size]
+        mini_batch = (X_minibatch, Y_minibatch)
+        mini_batches.append(mini_batch)
+    
+    if m%minibatch_size != 0:
+        X_minibatch = X_shuffled[:, full_minibatch_count*minibatch_size:]
+        Y_minibatch = Y_shuffled[:, full_minibatch_count*minibatch_size:]
+        mini_batch = (X_minibatch, Y_minibatch)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+
 
 def forward_propagation_for_predict(X, parameters):
 
@@ -137,3 +167,61 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     X = Activation('relu')(X)
     
     return X
+
+# GRADED FUNCTION: ResNet50
+
+def ResNet50(input_shape = (64, 64, 3), classes = 6):
+
+    # padding -> [conv 7*7 64, batchNorm, relue, maxpool] -> [conv_res -> 2 X identity_res]
+    # -> [conv_res -> 3 X identity_res] -> [conv_res -> 5 X identity_res] -> [conv_res -> 2 X identity_res]
+    # -> AveragePool -> Softmax_Dense
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    # Stage 3
+    X = convolutional_block(X, f = 3, filters=[128, 128, 512], stage = 3, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'b')
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'c')
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'd')
+
+    # Stage 4
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'b')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'c')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'd')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'e')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'f')
+
+    # Stage 5
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'b')
+    X = identity_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'c')
+
+    # AVGPOOL
+    X = AveragePooling2D((2, 2), name = 'avg_pool')(X)
+    
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
